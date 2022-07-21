5b79627b5407621b880d06f2ca6d955e057c0eb0
Adolfo <adoteponap@gmail.com>
2022-07-20 23:14:51 -0500

    composer install

--- resources/views/administrator/index.blade.php
+++ resources/views/administrator/index.blade.php
@@ -1,5 +1,5 @@
 @extends('index')
-@section('title','PosTepo | Tablero de control')
+@section('title','PosTepo | Tablero de Control')
 @section('root')
 <div class="d-flex flex-column flex-root" id="app">
     <!--begin::Page-->
