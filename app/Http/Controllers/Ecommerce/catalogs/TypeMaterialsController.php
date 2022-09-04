da817e315f172f5ea30fcb3cc68c015a361e117f
sheyla <sheylayomira@nixdit.com>
2022-09-04 17:56:07 -0500

    Act.2 Tipo de material

--- app/Http/Controllers/Ecommerce/catalogs/TypeMaterialsController.php
+++ app/Http/Controllers/Ecommerce/catalogs/TypeMaterialsController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers\Ecommerce\catalogs;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\UnitMeasurement;
+use App\Http\Controllers\Controller;
 
 class TypeMaterialsController extends Controller
 {
     public function index () {
-        return view("ecommerce.catalogs.type_materials.index");
+        $material = UnitMeasurement::all(); //Mandar a llamar todos los registros  
+        return view("ecommerce.catalogs.type_materials.index")->with("material_data",$material); //Pasar a la vista declarada
     }
 }
--- resources/views/ecommerce/catalogs/type_materials/index.blade.php
+++ resources/views/ecommerce/catalogs/type_materials/index.blade.php
@@ -1,8 +1,28 @@
 @extends('administrator.index')
 @section('title','Tipo de material| PosNix')
 @section('content')
-
-<div>
-    Hola soy Tipo de material 
+<div class="container">
+    <div class="table-responsive">
+        <table class="table table-rounded table-striped border gy-7 gs-7">
+            <thead class="thead-dark">
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Creado</th>
+                    <th>Actualizar</th>
+                </tr>
+            </thead> 
+            <tbody>
+                @foreach($material_data as $material)
+                <tr>
+                    <td>{{$material->id}}</td>
+                    <td>{{$material->name}}</td>
+                    <td>{{$material->created_at ?? "Sin fecha"}}</td>    <!-- ??  ""(Para referenciar un texto cuando es null) -->
+                    <td>{{$material->updated_at ?? "Sin actualizar"}}</td> 
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 @endsection
